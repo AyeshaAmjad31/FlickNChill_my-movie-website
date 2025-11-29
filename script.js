2a965a7277b58075399cbef4212ed96774f37c55
Ayesha Amjad <ayeshamjad503@gmail.com>
2025-11-29 12:59:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -48,6 +48,12 @@ const movieData = [
         thumbnail: "3idiots.jpg",
         genres: ["comedy, drama"]
     },
+     {
+        title: "K.G.F. CHAPTER 1",
+        url:"https://lok-lok.cc/spa/videoPlayPage/movies/k-g-f-chapter-1-AoFyfdy6BZ?id=833863936263999064&type=/movie/detail&lang=en",
+        thumbnail: "kgf1.jpg",
+        genres: ["action, crime, drama"]
+    },
     {
         title: "THE SHADOW'S EDGE",
         url:"https://lok-lok.cc/spa/videoPlayPage/movies/the-shadows-edge-YsfCvPo4Xo6?id=5373583835403911072&type=/movie/detail&lang=en",
@@ -184,6 +190,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // If splash is skipped
     if (!appContainer.classList.contains('hidden')) renderMovies(movieData);
 });
+
 
 
 
